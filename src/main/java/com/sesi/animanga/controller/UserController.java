25eb6540212d084d96508769367c3b13d7a98664
Christian Gonzalez <sesimx@gmail.com>
2023-04-10 09:42:38 -0600

    circuit braeker

--- src/main/java/com/sesi/animanga/controller/UserController.java
+++ src/main/java/com/sesi/animanga/controller/UserController.java
@@ -3,6 +3,7 @@ package com.sesi.animanga.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sesi.animanga.data.entity.User;
 import com.sesi.animanga.service.UserService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 public class UserController {
+	
+	@Autowired
+	private CircuitBreakerFactory cbFactory;
 	
 	@Autowired
 	private UserService service;
@@ -21,10 +27,17 @@ public class UserController {
 		return service.findAll();
 	}
 	
+	@CircuitBreaker(name = "user", fallbackMethod = "alternative")
 	@GetMapping("/user/{id}")
 	public User getUserById(@PathVariable Long id) {
-		return service.findById(id);
+		return cbFactory.create("user").run(() -> service.findById(id), e -> alternative(id));
 	}
 	
+	
+	public User alternative(Long id) {
+		User user = new User();
+		user.setName("Usuario Alternativo");
+		return user;
+	}
 	
 }
